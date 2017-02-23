22a7381aa3ee37ac237e2a3cf69744d1f4976c05
PC20838\L20805 <ozguraltayim@gmail.com>
2017-02-23 17:54:34 +0200

    First Commit

---
+++ Simple Blog/Simple Blog/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Simple_Blog.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Admin/Users
+        public ActionResult index()
+        {
+            return Content("Index121213");
+        }
+
+        public ActionResult list()
+        {
+            return Content("Listeee");
+        }
+
+        public ActionResult control(int id=0)
+        {
+            return Content("kontrolum ="+id);
+        }
+    }
+}
